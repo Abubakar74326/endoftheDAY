83f24fa3cc187a12167aaf5fe88b30b7d677293b
abubakar <mabubakarbssg@gmail.com>
2022-02-23 07:28:21 -0500

    SOME MEMORIES

---
+++ main.py
@@ -0,0 +1,4 @@
+distt_abbottabad = ["PMA","Army burn hall","Baloch mess"]
+#print(distt_abbottabad )
+pakistan_military = {"hafizabad":"oil depo" , "abbottabad":distt_abbottabad}
+print(pakistan_military)
